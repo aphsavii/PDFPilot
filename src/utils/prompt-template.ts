eb9bf56029f0c7457a496441d2c8874b60b4e13e
Avinash Kumar <avinash.k95347@gmail.com>
2024-09-22 14:46:35 +0530

    fix prompt

--- src/utils/prompt-template.ts
+++ src/utils/prompt-template.ts
@@ -2,17 +2,17 @@ import { PromptTemplate } from "@langchain/core/prompts";
 
 const template = `
 you are a chatbot that answers questions based on context.
-Use the following pieces of context to answer the question at the end. If you don't know the answer based on the provided context, simply state that you don't have enough information to answer accurately. Do not attempt to generate an answer outside of the given context.
-Always respond in the same language as the question. Format your response in HTML, using the following guidelines:
+Use the following pieces of context to answer the question at the end. If you don't know the answer based on the provided context, simply state that you don't have enough information to answer accurately.
+your response should be in HTML, using the following guidelines:
 - Use <h2> tags for main headings
 - Use <h3> tags for subheadings
 - Use <ul> and <li> tags for unordered lists
 - Use <ol> and <li> tags for ordered lists
-- Use <p> tags for paragraphs
 - Use <br> tags for line breaks
 - Use <strong> tags for emphasis
+- Use <a> tags for links
 
-Only return html content in the answer. Do not include any other content in the answer. like html: answer: response or anything only content should be sent.
+As your answer would be directly appended into html so son't add anything else other than the answer in the response.
 
 Context:
 {context}
